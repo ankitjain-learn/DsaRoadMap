1603a679666ca1dcd436d50e635ffc1b3192f94c
Ankit Jain <ankitjain0296@gmail.com>
2024-07-21 22:26:53 +0530

    adding I/OP 21-07-24

---
+++ src/InputOuput.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.Scanner;
+
+public class InputOuput {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter first number- ");
+        int a= sc.nextInt();
+        System.out.print("Enter second number- ");
+        int b= sc.nextInt();
+        System.out.print("Enter third number- ");
+        int c= sc.nextInt();
+        int d=a+b+c;
+        System.out.println("Total= " +d);
+
+    }
+}
---
+++ src/Patterns.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Patterns
+{
+    public static void main(String[] args) {
+        //PatternOne
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
