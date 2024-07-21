b4b491dfb208db0a639e895bfebc420b6028f413
Ankit Jain <ankitjain0296@gmail.com>
2024-07-22 00:39:19 +0530

    adding Patterns.java 22-07-24

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DsaRoadMap
+To remove cached files 
+1. add in gitignore and execute below commands
+2. git rm --cached -r .idea
+
+# Patterns
+1. whenever symmetrical pattern comes break into 2 funtions
+2. in space , make formula for space , star, space which is 3 inner loop
+3. when starting reducing start , take j=0 to n-i or j=n-i your choice
--- src/Patterns.java
+++ src/Patterns.java
@@ -3,6 +3,10 @@ package src;
 public class Patterns
 {
     public static void main(String[] args) {
+        patternSeven(4);
+    }
+
+    private static void patternOne() {
         //PatternOne
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
@@ -11,4 +15,87 @@ public class Patterns
             System.out.println();
         }
     }
+    private static void patternTwo() {
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(i+1);
+            }
+            System.out.println();
+        }
+    }
+    private static void patternThree(int n) {
+        for(int i=0;i<n;i++){
+            int print=0;
+            for(int j=0;j<n-i;j++){
+                System.out.print(++print);
+            }
+            System.out.println();
+        }
+    }
+    private static void patternFour(int n) {
+        for(int i=1;i<=n;i++){
+            //space
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int l=1;l<=(2*i-1);l++){
+                System.out.print("*");
+            }
+            //space
+            for(int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    private static void patternFive(int n) {
+        for(int i=1;i<=n;i++){
+            //space
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int l=1;l<=2*(n-i)+1;l++){
+                System.out.print("*");
+            }
+            //space
+            for(int k=1;k<i;k++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    private static void patternSix() {
+        //PatternSix
+        int start=1;
+        for(int i=0;i<5;i++){
+            if(i%2==0) start=1;
+            else start=0;
+            for(int j=0;j<=i;j++){
+                System.out.print(start);
+                start=1-start; //flipping
+            }
+            System.out.println();
+        }
+    }
+    private static void patternSeven(int n){
+        for(int i=1;i<=n;i++){
+            //space
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            //star
+            for(int j=1;j<=2*n-2*i;j++){
+                System.out.print(" ");
+            }
+            //space
+            for(int k=1;k<=i;k++){
+                System.out.print(i-k+1);
+            }
+            System.out.println();
+        }
+    }
+
+    //DO 10 AND THEN START WITH 14-22
 }
